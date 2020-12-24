b9b053f600d4233e407851c796d2cc42fb855468
Won <trilliwon@gmail.com>
2020-12-24 17:56:42 +0900

    Add Employee

--- employee/src/main/java/payroll/EmployeeApplication.java
+++ employee/src/main/java/payroll/EmployeeApplication.java
@@ -1,13 +1,13 @@
-package com.trilliwon.myapp;
+package payroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MyappApplication {
+public class EmployeeApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(MyappApplication.class, args);
+        SpringApplication.run(EmployeeApplication.class, args);
     }
 
 }
